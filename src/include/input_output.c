b69883054ddfda488b5c13a66670cb51be7f8b49
TihonSotnikov <tikhonsotnikov@gmail.com>
2025-05-23 23:14:05 +0700

    aboba

---
+++ src/include/input_output.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "input_output.h"
+#include "imageproc.h"
+
+ImageProcStatus load_image(FILE* input_file, Image* image)
+{
+
+}
+
+ImageProcStatus save_image_png(FILE* output_file, Image* image)
+{
+
+}
+
+ImageProcStatus save_image_jpeg(FILE* output_file, Image* image)
+{
+
+}
+
+ImageProcStatus free_image_data(Image* image)
+{
+    
+}
---
+++ src/include/input_output.h
@@ -0,0 +1,12 @@
+#ifndef INPUT_OUTPUT
+#define INPUT_OUTPUT
+
+#include <stdio.h>
+#include "imageproc.h"
+
+ImageProcStatus load_image(FILE* input_file, Image* image);
+ImageProcStatus save_image_png(FILE* output_file, Image* image);
+ImageProcStatus save_image_jpeg(FILE* output_file, Image* image);
+ImageProcStatus free_image_data(Image* image);
+
+#endif
